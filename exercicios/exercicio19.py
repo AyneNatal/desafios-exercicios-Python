18eccdb8d95d2c5ff0b3689dbea7b348ada8822d
Lorrayne <77463370+AyneNatal@users.noreply.github.com>
2023-09-05 11:11:54 -0300

    Update exercicio19.py

--- exercicios/exercicio19.py
+++ exercicios/exercicio19.py
@@ -1,10 +1,12 @@
 """
 EXERCÍCIO 19) Função Recursiva
 
-Funções recursivas, são funções que chamam a si mesmas durante sua execução. As funções recursivas são frequentemente usadas para resolver problemas que têm uma estrutura recursiva natural, sabendo disso, crie uma função para retornar o N-ésimo número na sequência de Fibonaci.
+Funções recursivas, são funções que chamam a si mesmas durante sua execução. As funções recursivas são frequentemente usadas para resolver problemas que têm uma estrutura recursiva natural, 
+sabendo disso, crie uma função para retornar o N-ésimo número na sequência de Fibonaci.
 
 Dica:
-A sequência de Fibonacci é uma das sequências matemáticas mais conhecidas e tem aplicações em várias áreas da matemática, ciência e computação. Ela também aparece em fenômenos naturais e em muitos contextos artísticos. A sequência pode ser definida matematicamente como: F(0) = 0 F(1) = 1 F(n) = F(n-1) + F(n-2) para n > 1 Onde F(n) representa o n-ésimo número na sequência de Fibonacci.
+A sequência de Fibonacci é uma das sequências matemáticas mais conhecidas e tem aplicações em várias áreas da matemática, ciência e computação. Ela também aparece em fenômenos naturais e em 
+muitos contextos artísticos. A sequência pode ser definida matematicamente como: F(0) = 0 F(1) = 1 F(n) = F(n-1) + F(n-2) para n > 1 Onde F(n) representa o n-ésimo número na sequência de Fibonacci.
 """
 
 def fibonacci(n):
